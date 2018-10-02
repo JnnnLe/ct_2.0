dcfef325b180d695b74289cbd9a0a87cd369c629
JnnnLe <jnnnle.sf@gmail.com>
2018-10-02 15:52:56 -0700

    Hid API key and created Cmp directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import coinMarketCapApiKey from './Components/keys';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>API key works!</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ client/src/Components/keys.js
@@ -0,0 +1,7 @@
+/* Coin Market Cap API key
+0e08d4bf-e259-40c3-982c-74a8a0bc4b54
+*/
+
+const coinMarketCapApiKey = '0e08d4bf-e259-40c3-982c-74a8a0bc4b54';
+
+export default coinMarketCapApiKey;
